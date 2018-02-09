f6cac67af48e6a42f24414bb0eac5f0644e63af2
janjelonek <jelon94@gmail.com>
2018-02-09 15:52:50 +0100

    Modules and some routing

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,20 +3,38 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesComponent } from './pages.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
     {
       path: '',
-      component: DashboardComponent
+      component: PagesComponent,
+      children: [
+        {
+          path: '',
+          component: DashboardComponent
+        },
+        {
+          path: 'rooms',
+          component: RoomsComponent
+        }
+      ]
     }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [
+    DashboardComponent,
+    PagesComponent,
+    RoomsComponent
+  ]
 })
 
 export class PagesModule { }
